71fff540814b9bd75d1366c06e88bfdc7f5eda69
Jim Phillips <jamminjimi@gmail.com>
2023-08-12 15:34:29 -0400

    add show on map

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -4,9 +4,11 @@
 [QueryProperty("Monkey", "Monkey")]
 public partial class MonkeyDetailsViewModel : BaseViewModel
 {
-    public MonkeyDetailsViewModel() 
+    IMap map;
+
+    public MonkeyDetailsViewModel(IMap map) 
     { 
-        
+        this.map = map;
     }
 
     [ObservableProperty]
@@ -16,5 +18,24 @@ public partial class MonkeyDetailsViewModel : BaseViewModel
     async Task GoBackAsync()
     {
         await Shell.Current.GoToAsync(".."); // .. is go back -- ..?id=1 passes parameters
+    }
+
+    [RelayCommand]
+    async Task OpenMapAsync()
+    {
+        try
+        {
+            await map.OpenAsync(Monkey.Latitude, Monkey.Longitude,
+                new MapLaunchOptions
+                {
+                    Name = Monkey.Name,
+                    NavigationMode = NavigationMode.None
+                });
+        }
+        catch(Exception ex)
+        {
+            await Shell.Current.DisplayAlert("Error!", $"Unable to open map: {ex.Message}", "Ok");
+
+        }
     }
 }
