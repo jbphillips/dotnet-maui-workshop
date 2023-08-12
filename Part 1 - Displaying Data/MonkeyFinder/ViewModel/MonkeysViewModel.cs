9007025d728c226fc23ba7b4ef07edceff802fce
Jim Phillips <jamminjimi@gmail.com>
2023-08-12 13:52:58 -0400

    added basic geolocation

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -1,5 +1,5 @@
-﻿using Android.Views.Accessibility;
-using MonkeyFinder.Services;
+﻿using MonkeyFinder.Services;
+using System.Linq.Expressions;
 
 namespace MonkeyFinder.ViewModel;
 
@@ -8,19 +8,58 @@ public partial class MonkeysViewModel : BaseViewModel
     MonkeyService monkeyService;
     public ObservableCollection<Monkey> Monkeys { get; } = new ObservableCollection<Monkey>();
 
+    IConnectivity connectivity;
+    IGeolocation geolocation;
+
     // This allows you to get monkeys from MAUI xaml.
     // GetMonkeysAsync is a private call.
     // Dont need to do it this way anymore. Use the toolkit
     //public Command GetMonkeysCommand { get; }
 
-    public MonkeysViewModel(MonkeyService monkeyService)
+    public MonkeysViewModel(MonkeyService monkeyService, IConnectivity connectivity, IGeolocation geolocation)
     {
         Title = "Monkey Finder";
         this.monkeyService = monkeyService;
+        this.connectivity = connectivity;
+        this.geolocation = geolocation;
 
         // dont need to do it this way anymore. Use the toolkit and add attribute to the getmonkeys call [RelayCommand]
         //GetMonkeysCommand = new Command(async () => await GetMonkeysAsync());
 
+    }
+
+    [RelayCommand]
+    async Task GetClosestMonkeyAsync(Monkey monkey)
+    {
+        if(IsBusy || Monkeys.Count == 0) return;
+
+        try
+        {
+            var location = await geolocation.GetLastKnownLocationAsync();
+
+            if(location is null)
+            {
+                location = await geolocation.GetLocationAsync(
+                    new GeolocationRequest
+                    {
+                        DesiredAccuracy = GeolocationAccuracy.Medium,
+                        Timeout = TimeSpan.FromSeconds(30)
+                    });
+            }
+
+            if (location is null) return;
+
+            var first = Monkeys.OrderBy(x => location.CalculateDistance(x.Latitude, x.Longitude, DistanceUnits.Miles)).FirstOrDefault();
+
+            if(first is null) return;
+
+            await Shell.Current.DisplayAlert("Closest Monkey", $"{first.Name} in {first.Location}", "Ok");
+        }
+        catch(Exception ex)
+        {
+            // this UI stuff should be in an interface or somethingc. Just doing here for this exercise
+            await Shell.Current.DisplayAlert("Error!", $"Unable to closest monkey: {ex.Message}", "Ok");
+        } 
     }
 
     [RelayCommand]
@@ -51,6 +90,11 @@ public partial class MonkeysViewModel : BaseViewModel
 
         try
         {
+            if(connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                await Shell.Current.DisplayAlert("Internet Issue", $"Check your internet and try again", "Ok");
+            }
+
             IsBusy = true;
             var monkeys = await monkeyService.GetMonkeys();
 
